637be22912090a4811d9cfd916de01215e76078b
byrneg7 <byrneg7@tcd.ie>
2020-04-27 18:20:45 +0100

    Work on landing page

--- client/src/Layouts/main/LandingPageLayout.js
+++ client/src/Layouts/main/LandingPageLayout.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import {Button} from '@material-ui/core'
 
-import './mainLayout.scss'
+import './landingPageLayout.scss'
 
 const LandingPageLayout = ({children}) => {
     return (
-        <div className="wrapper">
+        <div className="landing-page-wrapper">
             <header className="page-header">
                 header
             </header>
-            <main className="page-main">
+            <main className="landing-page-main shadow-sm">
+
+                <div className='landing-page-text-container font-huge font-weight-bold'>
+                    <div className='ml-3 mr-4'>
+                        Welcome text
+                    </div>
+                </div>
+
+                <div className='landing-page-button-container'>
+                    <Button variant="contained" className='shadow-md mb-2 mb-sm-0 d-block d-sm-inline-block banner-button mr-1'>
+                        Get Started
+                    </Button>
+
+                    <Button variant="contained" className='shadow-md mr-2 d-block d-sm-inline-block banner-button ml-1' color="primary">
+                        Sign Up
+                    </Button>
+                </div>
+
+            </main>
+            <main className="landing-page-secondary">
                 {children}
             </main>
             <footer className="page-footer">
---
+++ client/src/components/navigation/MainNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainNav = () => {
+    return (
+        <div>
+            Login
+        </div>
+    )
+};
+
+export default MainNav;
