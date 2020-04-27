91b289165ff9c340fb644730eedfc7089d05c570
byrneg7 <byrneg7@tcd.ie>
2020-04-27 19:05:30 +0100

    Landing page scaffolding

--- client/src/Layouts/main/LandingPageLayout.js
+++ client/src/Layouts/main/LandingPageLayout.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import {Button} from '@material-ui/core'
+import {withRouter} from "react-router-dom";
 
 import './landingPageLayout.scss'
+import MainNav from "../../components/navigation/MainNav";
 
-const LandingPageLayout = ({children}) => {
+const LandingPageLayout = ({children, history}) => {
     return (
         <div className="landing-page-wrapper">
-            <header className="page-header">
-                header
+            <header className="landing-page-header">
+                <MainNav/>
             </header>
+
             <main className="landing-page-main shadow-sm">
 
                 <div className='landing-page-text-container font-huge font-weight-bold'>
-                    <div className='ml-3 mr-4'>
-                        Welcome text
-                    </div>
+                    Lorem Ipsum
                 </div>
 
                 <div className='landing-page-button-container'>
-                    <Button variant="contained" className='shadow-md mb-2 mb-sm-0 d-block d-sm-inline-block banner-button mr-1'>
+                    <Button variant="contained"
+                            className='shadow-md mb-2 mb-sm-0 d-block d-sm-inline-block banner-button mr-1'
+                            onClick={() => history.push('/get-started')}>
                         Get Started
                     </Button>
 
-                    <Button variant="contained" className='shadow-md mr-2 d-block d-sm-inline-block banner-button ml-1' color="primary">
+                    <Button variant="contained" className='shadow-md mr-2 d-block d-sm-inline-block banner-button ml-1'
+                            color="secondary" onClick={() => history.push('/signup')}>
                         Sign Up
                     </Button>
                 </div>
 
             </main>
+
             <main className="landing-page-secondary">
                 {children}
             </main>
+
             <footer className="page-footer">
                 footer
             </footer>
@@ -38,4 +44,4 @@ const LandingPageLayout = ({children}) => {
     )
 };
 
-export default LandingPageLayout;
+export default withRouter(LandingPageLayout);
